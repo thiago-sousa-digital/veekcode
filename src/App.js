b36f7cbe859cadd85833ed5790a5e938c050d4dc
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-01-31 18:56:07 -0200

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,162 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div id="white_container" />
+        <section id="ddd-section">
+          <div id="ddd"><textarea id="rounded-rectangle-6-copy-4" placeholder="SEU DDD" name="SEU DDD" autofocus defaultValue={""} />
+            <p id="see-if-your-ddd-already">Veja se seu DDD já está disponível:</p><button id="rounded-rectangle-6-copy-2"><p id="buscar">BUSCAR</p></button></div>
+          <div id="banner"><img id="rectangle-23-2" src="images/rectangle-23.png" alt />
+            <a id="link-rectangle-23-copy-2" className="rapid-arrow"><img id="rectangle-23-copy-2" src="images/rectangle-23-copy.png" alt /></a>
+            <div id="ellipse-2" />
+            <div id="ellipse-2-copy" />
+            <div id="ellipse-2-copy-2" />
+            <div id="ellipse-2-copy-3" />
+            <div id="txt">
+              <p id="you-ask-for-your--nbsp-veekchip">VOCÊ PEDE SEU&nbsp; VEEKCHIP, CARREGA<br />COM SEU CARTÃO DE CRÉDITO E USA&nbsp; <br />COMO QUISER.</p>
+              <div id="rectangle-22-copy" />
+              <div id="rectangle-22" />
+            </div><img id="header" src="images/l-ogoheader.png" alt="l ogoheader" />
+            <div id="lojas">
+              <p id="download-the-app-and-play-2">BAIXE O APP E PEÇA SEU VEEKCHIP.<br />VOCÊ NÃO VAI PAGAR NADA =)</p>
+              <div id="bt_googleplay">
+                <p id="google-play-2">Google play</p><img id="shape-1-copy-2" src="images/shape-1-copy.png" alt="shape 1 copy" /><button id="rounded-rectangle-6-copy-3-4" />
+                <p id="app-store-2">App Store</p><img id="forma-1-copy-2" src="images/forma-1-copy_1.png" alt="forma 1 copy" /><button id="rounded-rectangle-6-copy-3-3" /></div>
+            </div>
+            <div id="flutuante"><button id="rounded-rectangle-6-copy-13"><img id="shape-1-copy-3" src="images/shape-1-copy_2.png" alt="shape 1 copy" /><p id="google-play-3">Google play</p></button>
+              <div id="group-17">
+                <p id="download-the-app-and-play-3">BAIXE O APP E PEÇA SEU CHIP DE GRAÇA!</p><button id="rounded-rectangle-6-copy-14"><img id="forma-1-copy-3" src="images/forma-1-copy_2.png" alt="forma 1 copy" /><p id="app-store-3">App Store</p></button><img id="vector-smart-object-copy-2" src="images/vector-smart-object-copy-2.png" alt /></div>
+            </div>
+          </div>
+        </section>
+        <section id="txt-3-section" />
+        <section id="use-indique-e-ganhe-section"><button id="rounded-rectangle-6-copy-10"><p id="a-gente-te-conta-mais">A GENTE TE CONTA MAIS POR AQUI</p></button>
+          <p id="use-enter-and-win"><span id="text1">Use, indique e ganhe.<br /></span><span id="text2">Se você descobriu a Veek, gostou e quer indicar para seus amigos, a gente te dá uma recompensa por isso. Justo né?<br />Quanto mais pessoas você indicar e mais recargas eles fizerem, mais VeekCoins você pode ganhar.</span></p>
+        </section>
+        <section id="como-voc-pode-usar-section">
+          <div id="como-voc-pode-usar">
+            <div id="_30-2" />
+            <div id="_60-2" />
+            <p id="how-you-can-use">Como você pode usar:</p>
+            <p id="confira-nossas-tarifas">CONFIRA NOSSAS TARIFAS</p><img id="rectangle-17-copy-2" src="images/rectangle-17-copy-2.png" alt />
+            <div id="ellipse-1"><img id="rectangle-17-copy" src="images/rectangle-17-copy.png" alt /><img id="rectangle-17" src="images/rectangle-17.png" alt /></div>
+            <div id="rounded-rectangle-7-copy" />
+            <div id="rounded-rectangle-7" />
+            <p id="_30-min">30 min</p>
+            <p id="_10gb">10GB</p>
+          </div>
+          <div id="planos"><img id="vector-smart-object-5" src="images/vector-smart-object_4.png" alt />
+            <div id="rectangle-16-copy"><img id="vector-smart-object-4" src="images/vector-smart-object_3.png" alt />
+              <p id="plans-only-if-it-is"><span id="text3">Planos? Só se for de conquistar o mundo.<br /></span><span id="text4">Na Veek, você paga só pelo que realmente usa. Sem pegadinha. Sem texto de rodapé.</span></p>
+            </div>
+          </div>
+          <div id="destaque-60--90">
+            <div id="rectangle-23">
+              <p id="carregue">CARREGUE</p>
+            </div>
+            <div id="rectangle-23-copy">
+              <p id="receba">RECEBA</p>
+            </div>
+            <p id="sua-recarga-vale-muito-mais">SUA RECARGA VALE MUITO MAIS AQUI!</p><img id="rectangle-20-copy" src="images/rectangle-20-copy.png" alt /><img id="rectangle-20-copy-2" src="images/rectangle-20-copy-2.png" alt />
+            <p id="r60">R$60</p>
+            <p id="v--90">V$90</p>
+          </div>
+          <div id="footer">
+            <p id="veek-2017---todos">© Veek 2017 — Todos os direitos reservados</p>
+            <p id="contrato-do-usurioguia-da-veektermos">CONTRATO DO USUÁRIO<br />GUIA DA VEEK<br />TERMOS E CONDIÇÕES<br />POLÍTICA DE PRIVACIDADE</p>
+            <p id="quer-saber-mais-sobre-a">QUER SABER MAIS SOBRE A VEEK?</p>
+            <p id="download-the-app-and-play">BAIXE O APP E PEÇA SEU CHIP DE GRAÇA!</p>
+            <p id="coisinhas-importantes">COISINHAS IMPORTANTES</p><button id="rounded-rectangle-10"><img id="vector-smart-object" src="images/vector-smart-object.png" alt /></button><button id="rounded-rectangle-10-copy"><img id="forma-1" src="images/forma-1.png" alt="forma 1" /></button>
+            <button id="rounded-rectangle-10-copy-2"><img id="vector-smart-object-2" src="images/vector-smart-object_1.png" alt /></button><button id="rounded-rectangle-10-copy-2-2"><img id="vector-smart-object-3" src="images/vector-smart-object_2.png" alt /></button><img id="rectangle-17-copy-3" src="images/rectangle-17-copy-3.png" alt /><button id="contact-us-">FAÇA SEU LOGIN</button>
+            <p id="desbravador">DESBRAVADOR?</p>
+            <p id="doubts">DÚVIDAS?</p><img id="rectangle-17-copy-7" src="images/rectangle-17-copy-7.png" alt /><img id="rectangle-17-copy-4" src="images/rectangle-17-copy-4.png" alt /><img id="rectangle-17-copy-5" src="images/rectangle-17-copy-5.png" alt />
+            <img id="rectangle-17-copy-6" src="images/rectangle-17-copy-6.png" alt />
+            <div id="lojas-copy">
+              <p id="google-play">Google play</p><img id="shape-1-copy" src="images/shape-1-copy.png" alt="shape 1 copy" /><button id="rounded-rectangle-6-copy-3-2" />
+              <p id="app-store">App Store</p><img id="forma-1-copy" src="images/forma-1-copy.png" alt="forma 1 copy" /><button id="rounded-rectangle-6-copy-3" /></div>
+            <p id="_3003-1246">3003 1246</p>
+          </div>
+        </section>
+        <section id="atendimento-section">
+          <div id="atendimento">
+            <p id="queue-for-what"><span id="text5"> Fila pra quê?<br /></span><span id="text6">Atendimento que funciona!<br /><br />Chat no app, atendimento nas redes sociais, e-mail e telefone.<br />Nosso time de atendimento está sempre pronto para resolver o que você precisar.<br /><br />Com dúvidas?</span></p>
+            <img id="vector-smart-object-6" src="images/vector-smart-object_5.png" alt />
+            <div id="rectangle-28">
+              <div id="rectangle-27" /><button id="rounded-rectangle-6-copy-9"><p id="our-faq-helps-you--nbsp-">NOSSO FAQ TE AJUDA&nbsp; =)</p></button><button id="our-faq-helps-you--nbsp-">NOSSO FAQ TE AJUDA&nbsp; =)</button></div>
+          </div>
+          <div id="app">
+            <div id="iphone-6-47">
+              <div id="base">
+                <div id="_2-4" title={2}>
+                  <div id="_3">
+                    <div id="_4">
+                      <div id="sensor" />
+                      <div id="camera">
+                        <div id="camera-lense" />
+                      </div>
+                      <div id="microphone">
+                        <div id="microphone-base-2" />
+                        <div id="microphone-base" />
+                      </div>
+                      <div id="home-button">
+                        <div id="home-button-base" />
+                      </div>
+                      <div id="screen"><img id="place-your-screen-here" src="images/place-your-screen-here.png" alt="place your screen here" /></div>
+                    </div>
+                  </div>
+                </div>
+                <div id="div-5">
+                  <div id="div-4" />
+                  <div id="div-3" />
+                  <div id="div-2" />
+                  <div id="div" />
+                </div>
+              </div>
+              <div id="buttons"><img id="volume-button" src="images/volume-button.png" alt="volume button" />
+                <div id="button"><img id="_1-3" src="images/_1_2.png" alt={1} />
+                  <div id="_2-3" />
+                </div>
+                <div id="obj"><img id="_1-2" src="images/_1_1.png" alt={1} />
+                  <div id="_2-2" />
+                </div>
+                <div id="button-2"><img id="_1" src="images/_1.png" alt={1} />
+                  <div id="_2" />
+                </div>
+              </div>
+            </div>
+            <div id="recarga-facil">
+              <div id="icon_recarga">
+                <div id="ellipse-4-copy-5"><img id="shape-1-2" src="images/shape-1_1.png" alt="shape 1" /></div>
+                <div id="ellipse-4-copy-2" />
+              </div>
+              <p id="easy-recharge"><span id="text13">Recarga fácil.<br /></span><span id="text14">Com seu cartão de crédito, <br />você recarrega quando quiser ou configura uma recarga automática para não ter que se preocupar com seu saldo ou validade.</span></p>
+            </div>
+            <div id="traz-seu-nmero">
+              <p id="bring-your-number-to"><span id="text11">Traz o seu número pra cá.<br /></span><span id="text12">Ah! Quer trazer seu número antigo? <br />Você consegue também!</span></p>
+              <div id="icon_port">
+                <div id="ellipse-4-copy-4"><img id="shape-1" src="images/shape-1.png" alt="shape 1" /></div>
+                <div id="ellipse-4-copy" />
+              </div><button id="bt_comofunciona"><p id="como-funciona-a-portabilidade">COMO FUNCIONA A PORTABILIDADE</p></button></div>
+            <div id="tudo-direto-no-app">
+              <p id="everything-straight-into-the-app"><span id="text9">Tudo direto no app!<br /></span><span id="text10">Faça a ativação do seu chip, receba seu número e já saia usando!</span></p>
+              <div id="icon_cel">
+                <div id="ellipse-4-copy-3"><img id="rounded-rectangle-9" src="images/rounded-rectangle-9.png" alt /></div>
+                <div id="ellipse-4" />
+              </div><button id="bt_duvida"><p id="doubt-on-activation-click-here">DÚVIDA NA ATIVAÇÃO? CLICA AQUI.</p></button></div>
+            <p id="tudo-na-palma-da-sua"><span id="text7">Tudo na palma da sua mão!<br /></span><span id="text8">Um APP que resolve a sua vida, de verdade!</span></p><img id="layer-30" src="images/layer-30.png" alt /><img id="layer-29-copy" src="images/layer-29-copy.png" alt /></div>
+        </section>
+        <div id="txt-3">
+          <p id="_60">60</p>
+          <p id="_90-veekcoins">90 VeekCoins</p>
+          <p id="r-2">R$</p>
+        </div>
+        <div id="txt-2">
+          <p id="_30">30</p>
+          <p id="_30-veekcoins">30 VeekCoins</p>
+          <p id="r">R$</p>
+        </div>
       </div>
     );
   }
